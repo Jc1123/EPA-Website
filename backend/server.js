f1d37511a10337352b8dcd7150d173bbde2326e5
João Carlos <dkopeer@gmail.com>
2026-09-22 12:50:35 -0300

    Alterado server.json

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes.js';
 import recruitRoutes from './routes/recruit.routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-// Rotas da API
+// Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/recruit', recruitRoutes);
 
+// Executa o escutador de porta apenas em ambiente local
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Servidor a correr na porta ${PORT}`));
+}
+
+// Exportação obrigatória para a Vercel funcionar como Serverless
 export default app;
